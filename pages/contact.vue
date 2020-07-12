876b1390d42cb1e27a1102b29009957c932bd698
Caleb Spain <crspain@uh.edu>
2020-07-12 17:30:26 -0500

    Updated Contact Page

--- pages/contact.vue
+++ pages/contact.vue
@@ -7,19 +7,41 @@
       <v-col cols="12">
         <v-card class="contact-card mx-auto" max-width="650">
           <v-card-text class="pa-0">
-            <v-text-field label="Name" single-line outlined></v-text-field>
-            <v-text-field label="Email address" single-line outlined></v-text-field>
-            <v-text-field label="Subject" single-line outlined></v-text-field>
-            <v-textarea outlined label="Your message" rows="4"></v-textarea>
+            <v-text-field v-model="name" label="Name" single-line outlined></v-text-field>
+            <v-text-field v-model="address" label="Email address" single-line outlined></v-text-field>
+            <v-text-field v-model="subject" label="Subject" single-line outlined></v-text-field>
+            <v-textarea v-model="message" outlined label="Your message" rows="4"></v-textarea>
           </v-card-text>
           <v-card-actions class="pa-0">
-            <v-btn large class="mb-4 px-0" style="width:100%">Send</v-btn>
+            <v-btn @click="sendEmail" large class="mb-4 px-0" style="width:100%">Send</v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      address: "",
+      subject: "",
+      message: ""
+    };
+  },
+  methods: {
+    sendEmail() {
+      this.name = "";
+      this.address = "";
+      this.subject = "";
+      this.message = "";
+    }
+  }
+};
+</script>
+
 <style scoped>
 .contact-card {
   box-shadow: none !important;
